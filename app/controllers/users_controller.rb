8e11890e36a8056c0b8c5604c12992c3996ca382
github username <github email address>
2019-09-05 16:43:53 -0700

    Strong params for users, added email field in signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:name, :email, :password, :course_level)
   end
 end
