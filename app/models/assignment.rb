995f0f5a95dff4229b07aba3214c394a362a3385
github username <github email address>
2019-09-05 10:30:31 -0700

    Created associations for material and assignment models

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,2 +1,4 @@
 class Assignment < ApplicationRecord
+  belongs_to :material
+  belongs_to :student
 end
--- app/models/material.rb
+++ app/models/material.rb
@@ -1,2 +1,4 @@
 class Material < ApplicationRecord
+  has_many :assignments
+  has_many :students, through: :assignments
 end
