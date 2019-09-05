ceb34b921195c52ec49816f2d86dcce90819ea17
github username <github email address>
2019-09-05 10:29:11 -0700

    Created associations for student and teacher models

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,6 @@
 class Student < ApplicationRecord
+  has_many :student_teachers
+  has_many :teachers, through: :student_teachers
+  has_many :assignments
+  has_many :materials, through: :assignments
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,4 @@
 class Teacher < ApplicationRecord
+  has_many :student_teachers
+  has_many :students, through: :student_teachers
 end
