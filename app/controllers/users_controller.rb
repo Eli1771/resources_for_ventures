e7a446a2aef960197a4c5491c0b3bca6ad52ad15
github username <github email address>
2019-09-05 16:49:52 -0700

    Post routes for signup/signin, stubbed user#create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,14 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.create(user_params)
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
   end
 
   private
 
-  def users_params
+  def user_params
     params.require(:user).permit(:name, :email, :password, :course_level)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :teachers, only: [:index, :show]
 
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get '/signin', to: 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
