904305af7f3b02019a29f5c82ff57c86c1f19ca4
github username <github email address>
2019-09-06 13:07:23 -0700

    Starting materials veiws/controller actions

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,2 +1,16 @@
 class MaterialsController < ApplicationController
+  def new
+  end
+
+  def create
+    
+  end
+
+  def index
+    @materials = Material.all
+  end
+
+  def show
+    @material = Material.find(params[:id])
+  end
 end
