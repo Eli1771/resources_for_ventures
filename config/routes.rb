b678f87925c27b6dcf086fe87c38b20e7cd24af0
github username <github email address>
2019-09-06 10:50:52 -0700

    Stubbed controllers and views for student/teacher show pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   get '/signout', to: 'sessions#destroy'
+
+  get '/students/:id', to: 'students#show'
+  get '/teachers/:id', to: 'teachers#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
