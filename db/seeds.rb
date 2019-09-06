4e851a1334b556e079c99f6e888998facd0997f2
github username <github email address>
2019-09-06 15:41:35 -0700

    Wrote users in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,26 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 levels = ['Basic', 'Level 1', 'Level 2', 'Level 3', 'Level 4']
 
+p = 'Lando'
+users = [
+  {name: 'Eli', email: 'eli@fake.com', password: p, course_id: 3, is_teacher: true},
+  {name: 'Aubrey', email: 'aubrey@fake.com', password: p, course_id: 1, is_teacher: true},
+  {name: 'Jimmy', email: 'jimmy@fake.com', password: p, course_id: 3, is_teacher: false},
+  {name: 'Mark', email: 'mark@fake.com', password: p, course_id: 3, is_teacher: false},
+  {name: 'Sue', email: 'sue@fake.com', password: p, course_id: 1, is_teacher: false},
+  {name: 'Barry', email: 'barry@fake.com', password: p, course_id: 1, is_teacher: false},
+  {name: 'Larry', email: 'larry@fake.com', password: p, course_id: 2, is_teacher: false},
+  {name: 'Harry', email: 'harry@fake.com', password: p, course_id: 5, is_teacher: false},
+  {name: 'Phillip', email: 'phillip@fake.com', password: p, course_id: 1, is_teacher: false},
+  {name: 'Reggie', email: 'reggie@fake.com', password: p, course_id: 5, is_teacher: false},
+  {name: 'Denise', email: 'denise@fake.com', password: p, course_id: 3, is_teacher: false},
+  {name: 'Marty', email: 'marty@fake.com', password: p, course_id: 4, is_teacher: false}
+]
+
 levels.each do |level|
   Course.create(name: level)
+end
+
+users.each do |user|
+  User.create(user)
 end
