55528294ea17cc61b06ed3b5b84c99e47725177f
github username <github email address>
2019-09-06 10:26:59 -0700

    Basic signout working

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   post '/users/new', to: 'users#create'
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
+  get '/signout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
