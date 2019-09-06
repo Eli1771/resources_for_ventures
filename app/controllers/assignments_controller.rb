1e443c2b7eef6d77b42d7a7934129b1b080e9329
github username <github email address>
2019-09-06 13:45:51 -0700

    New assignment form working in controller

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,3 +1,15 @@
 class AssignmentsController < ApplicationController
-  
+  def new
+  end
+
+  def create
+    @assignment = Assignment.create(assignment_params)
+    redirect_to assignment_path(@assignment)
+  end
+
+  private
+
+  def assignment_params
+    params.require(:assignment).permit(:student_id, :material_id, :mark_as_viewed)
+  end
 end
