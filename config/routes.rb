3c698175af4ea5e5abec067ae4d7fbf6aae01acf
github username <github email address>
2019-09-05 20:15:33 -0700

    Course field is now a collection_select

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users
 
   get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
+  post '/users/new', to: 'users#create'
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
