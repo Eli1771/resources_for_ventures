5876ad3806b3f96d14606199f54536a3399a14e9
github username <github email address>
2019-09-07 06:38:31 -0700

    Scoping teacher routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
   resources :assignments, only: [:index, :show, :new, :create]
-  resources :materials, only: [:index, :show, :new, :create]
+  resources :materials, only: [:index, :show]
   resources :users, only: [:index, :show, :new, :create]
 
   get '/signup', to: 'users#new'
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
 
   get '/students', to: 'students#index'
   get '/teachers', to: 'teachers#index'
+  scope :teachers do
+    resources :materials, only: [:new, :create, :edit, :update]
+    resources :assignments, only: [:new, :create]
+  end
   get '/students/:id', to: 'students#show'
   get '/teachers/:id', to: 'teachers#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
