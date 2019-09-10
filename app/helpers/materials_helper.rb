64dabd380e33fc3fc00e164caaf3e76e5faeb607
github username <github email address>
2019-09-10 14:55:25 -0700

    Auto-link to proper new material form

--- app/helpers/materials_helper.rb
+++ app/helpers/materials_helper.rb
@@ -1,11 +1,17 @@
 module MaterialsHelper
   def course_id_field(material)
     if material.course.nil?
-      #course select field
       select_tag 'material[course_id]', options_from_collection_for_select(Course.all, :id, :name)
     else
-      #hidden field with course_id
       hidden_field_tag 'material[course_id]', material.course_id
+    end
+  end
+
+  def correct_new_material_path
+    if params[:course_id]
+      return new_course_material_path
+    else
+      return new_material_path
     end
   end
 end
