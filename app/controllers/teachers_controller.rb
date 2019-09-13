d3cb7b787a9f5018a103f70ec15401117825b31c
github username <github email address>
2019-09-12 19:08:00 -0700

    Basic flash working

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -6,6 +6,19 @@ class TeachersController < ApplicationController
   def show
     @teacher = User.find(params[:id])
     @assignments = Assignment.all.select {|a| a.creator_id == @teacher.id}
-    @teacher.redirect_if_not_teacher
+    redirect_if_not_teacher(current_user)
+  end
+
+  private
+
+  def current_user
+    User.find(session[:user_id])
+  end
+
+  def redirect_if_not_teacher(user)
+    if user.is_teacher == false
+      flash[:message] = 'Sorry, students don\'t have permission to view that page'
+      redirect_to user.correct_user_path
+    end
   end
 end
