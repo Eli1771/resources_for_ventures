5c68686633f52d5b1086aa3b2fc2f6b0e53abc0b
github username <github email address>
2019-09-19 13:08:26 -0700

    Omniauth callback route not allowed to be in other controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,18 +9,20 @@ class UsersController < ApplicationController
   def create
     binding.pry
     @user = User.new(user_params)
-    if @user.save
+    if !!params[:facebook]
+      redirect_to '/auth/facebook/callback'
+    elsif @user.save
       session[:user_id] = @user.id
       flash[:success] = "Welcome, #{@user.name}! Successfully signed up!"
       redirect_to @user.correct_user_path
-    elsif params[:facebook]
-      redirect_to '/auth/facebook/callback'
     else
       render :new
     end
   end
 
   def fb_create
+    binding.pry
+    @user = User.all.last
     @user = User.find_or_create_by(uid: auth['uid']) do |u|
       u.name = auth['info']['name']
       u.email = auth['info']['email']
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  validates :name, { presence: true }
-  validates :email, { presence: true }
-  validates :password_digest, { presence: true }
-  validates :course_id, {presence: true}
+  # validates :name, { presence: true }
+  # validates :email, { presence: true }
+  # validates :password_digest, { presence: true }
+  # validates :course_id, {presence: true}
 
   has_many :assignments
   has_many :materials, through: :assignments
