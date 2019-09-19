acda3f0a0fe7a667e60d4daf559457ffa76370bf
github username <github email address>
2019-09-19 12:30:24 -0700

    Starting to route fbcreate through original users#create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    binding.pry
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = "Welcome, #{@user.name}! Successfully signed up!"
       redirect_to @user.correct_user_path
+    elsif params[:facebook]
+      redirect_to '/auth/facebook/callback'
     else
       render :new
     end
